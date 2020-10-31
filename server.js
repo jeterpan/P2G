95fe8f231f3adb3b8d6181c038c7eb8df764f077
JeterPan <41131222+jeterpan@users.noreply.github.com>
2020-10-31 13:40:28 -0300

    Changing game URL

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get('/', function (req, res) {
 })
 
 //app.post('/api/game/:event?/:room?/:player?', function (req, res) {
-app.post('/api/game?event=details&room=1player=Pedro', function (req, res) {
+app.post('/api/game', function (req, res) {
     
   global.context = url.parse(req.url,true).query
 
@@ -88,7 +88,7 @@ io.sockets.on('connection', socket => {
           .to(user.room)
           .emit(
             'message',
-            formatMessage('God', `"event": "${context.event}"`)
+            formatMessage('backend', `${context.event}`)
           );
         } else {
           //const user = getUserByName(global.context.room)
